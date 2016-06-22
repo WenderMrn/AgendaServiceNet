e348d55c597265158698cbb1510e581e7e7fc5a8
Wender Mrn <wender.mrn@gmail.com>
2016-06-22 19:52:37 -0300

    criando a interface DAO

---
+++ models/DAO/DAO.interface.php
@@ -0,0 +1,9 @@
+<?php 
+	interface IDAO{
+		public function create($obj);
+		public function read($chave);
+		public function update($obj);
+		public function delete($obj) ;
+		public function refresh($obj);
+	}
+?>
